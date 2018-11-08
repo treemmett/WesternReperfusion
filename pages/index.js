cf15f9db28195347d4af9aab5d5b50d432f9d153
treemmett <tregan@tregan.me>
2018-11-08 13:01:20 -0700

    Adds parallax effect

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,48 @@
 import Page from '../layouts/main';
+import { Parallax } from 'react-parallax';
 import './index.scss';
 
 export default () => (
   <Page>
     <div className="hero">
       <div className="tagline">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
+      <Parallax
+      bgImage="/static/images/hero1.jpg"
+      strength={150}
+      blur={0.1}
+    />
     </div>
     <section>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Maecenas ornare, mi id aliquet sollicitudin, eros lorem tempor urna, non auctor.
+        Sed at viverra turpis.
+        Quisque rhoncus, tellus non facilisis interdum, libero tortor consectetur mauris, vel volutpat orci arcu vel leo.
+        Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+        Donec ultricies porttitor feugiat.
+        Nam egestas nibh.
       </p>
     </section>
+    <div className="hero">
+      <Parallax
+      bgImage="/static/images/hero2.jpg"
+      strength={150}
+      blur={0.1}/>
+    </div>
+    <section>
+      <p>
+        Aenean dignissim auctor nisi, non faucibus risus vehicula ut.
+        Sed imperdiet maximus dui et auctor.
+        Fusce dictum mi odio, non facilisis elit posuere sit amet.
+        Nullam erat justo, interdum non sagittis eu, venenatis sed leo.
+        Fusce eget sem magna.
+        Praesent laoreet rhoncus risus, ac lobortis diam condimentum vel.
+        Sed et augue commodo, scelerisque nisl ut, varius.
+      </p>
+    </section>
+    <div className="hero">
+      <Parallax
+        bgImage="/static/images/hero3.jpg"
+        strength={150}
+        blur={0.1}/>
+    </div>
   </Page>
 );
