a1ec01bc272b2ed9437ef235763846981e5ffb86
treemmett <tregan@tregan.me>
2018-11-07 23:11:45 -0700

    Adds social media links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,5 +10,12 @@ export default () => (
         <input type="submit" value="Sign Up"/>
       </form>
     </div>
+
+    <div className="social">
+      <a href="https://twitter.com/WReperfusion" target="_blank" rel="noopener noreferrer" className="tw"/>
+      <a href="https://www.youtube.com/channel/UCKTN8XmuSnfXU8inAeDm1hw" target="_blank" rel="noopener noreferrer" className="yt"/>
+      <a href="https://www.instagram.com/wreperfusion/" target="_blank" rel="noopener noreferrer" className="ig"/>
+      <a href="https://www.facebook.com/Western-Reperfusion-493696127803906" target="_blank" rel="noopener noreferrer" className="fb"/>
+    </div>
   </footer>
 );
